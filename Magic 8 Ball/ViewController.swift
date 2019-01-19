1003693a3273b7245f6a7e1ba8e47ad122a6e522
Frank Reichard <frankreichard10@gmail.com>
2019-01-19 10:13:54 -0500

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Reichard, Frank (US - Mechanicsburg Delivery) on 1/19/19.
+//  Copyright © 2019 Frank Reichard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do ay additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
